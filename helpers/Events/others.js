86f1bce80f61d9fccc873ea97b7f7e2dbd6306eb
Rifza123 <rifzabungodp@gmail.com>
2024-07-16 00:16:04 +0700

    update

--- helpers/Events/others.js
+++ helpers/Events/others.js
@@ -1,5 +1,6 @@
 /*!-======[ Module Imports ]======-!*/
 const fs = "fs".import();
+const { downloadContentFromMessage } = "baileys".import()
 
 /*!-======[ Default Export Function ]======-!*/
 export default async function on({ cht, Exp, store, ev, is }) {
@@ -20,7 +21,7 @@ export default async function on({ cht, Exp, store, ev, is }) {
                     body: "Artificial Intelligence, The beginning of the robot era",
                     thumbnail: fs.readFileSync(fol[3] + "bell.jpg"),
                     sourceUrl: "https://github.com/Rifza123",
-                    mediaUrl: "http://ẉa.me/62831109556302/"+Math.floor(Math.random() * 100000000000000000),
+                    mediaUrl: "http://ẉa.me/6283110928302/"+Math.floor(Math.random() * 100000000000000000),
                     renderLargerThumbnail: true,
                     showAdAttribution: true,
                     mediaType: 1,
@@ -34,6 +35,32 @@ export default async function on({ cht, Exp, store, ev, is }) {
             }
         }
         Exp.sendMessage(cht.id, menu, { quoted: cht })
+    })
+    
+    ev.on({ 
+        cmd: ['rvo','getviewonce'],
+        listmenu: ['getviewonce'],
+        tag: 'baileys',
+        energy: 25
+    }, async() => {
+       if (!(is?.owner || is?.admin)) return cht.reply("Maaf, males nanggepin. ini khusus owner/admin kalo di grup");
+         if(cht.mention.length < 1) return cht.reply("Reply/tag orangnya!")
+        let ab = store.messages[cht.id].array.filter(a => a.key.participant.includes(cht.mention[0]) && (a.message?.viewOnceMessageV2 || a.message?.viewOnceMessageV2Extension))
+        if(ab.length == 0) return cht.reply("Org itu tak pernah mengirimkan foto/video 1xlihat!")
+        for(let aa of ab){
+            let thay = {
+                msg: aa.message.viewOnceMessageV2?.message?.imageMessage || aa.message.viewOnceMessageV2?.message?.videoMessage || aa.message.viewOnceMessageV2Extension?.message?.audioMessage,
+                type: aa.message.viewOnceMessageV2?.message?.imageMessage ? "image" : aa.message.viewOnceMessageV2?.message?.videoMessage ? "video" : "audio"
+            }
+            let stream = await downloadContentFromMessage(thay.msg,thay.type)
+            let buffer = Buffer.from([])
+            for await (const chunk of stream) {
+                buffer = Buffer.concat([buffer, chunk])
+            }
+            let mssg = {}
+            thay.type == "audio" && (mssg.ptt = true)
+            await Exp.sendMessage(cht.id, {  [thay.type]: buffer, ...mssg }, { quoted:aa })
+        }
     })
 
 }
